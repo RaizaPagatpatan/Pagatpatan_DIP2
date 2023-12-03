9e3112a64b3f21472b06044cedbf5067f1c7cc42
Raiza Pagatpatan <rjimenezpagatpatan@gmail.com>
2023-12-03 18:54:02 +0800

    Add project files.

---
+++ Pagatpatan_DIP2/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Pagatpatan_DIP2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.btnToggleCamera = new System.Windows.Forms.Button();
+            this.btnLoadBackground = new System.Windows.Forms.Button();
+            this.btnSubtract = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(21, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(459, 309);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Location = new System.Drawing.Point(128, 348);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(100, 45);
+            this.btnUpload.TabIndex = 3;
+            this.btnUpload.Text = "Upload Image";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // btnToggleCamera
+            // 
+            this.btnToggleCamera.Location = new System.Drawing.Point(234, 348);
+            this.btnToggleCamera.Name = "btnToggleCamera";
+            this.btnToggleCamera.Size = new System.Drawing.Size(100, 45);
+            this.btnToggleCamera.TabIndex = 4;
+            this.btnToggleCamera.Text = "Use Camera";
+            this.btnToggleCamera.UseVisualStyleBackColor = true;
+            this.btnToggleCamera.Click += new System.EventHandler(this.btnToggleCamera_Click);
+            // 
+            // btnLoadBackground
+            // 
+            this.btnLoadBackground.Location = new System.Drawing.Point(638, 348);
+            this.btnLoadBackground.Name = "btnLoadBackground";
+            this.btnLoadBackground.Size = new System.Drawing.Size(100, 45);
+            this.btnLoadBackground.TabIndex = 5;
+            this.btnLoadBackground.Text = "Load Background";
+            this.btnLoadBackground.UseVisualStyleBackColor = true;
+            this.btnLoadBackground.Click += new System.EventHandler(this.btnLoadBackground_Click);
+            // 
+            // btnSubtract
+            // 
+            this.btnSubtract.Location = new System.Drawing.Point(1100, 348);
+            this.btnSubtract.Name = "btnSubtract";
+            this.btnSubtract.Size = new System.Drawing.Size(100, 45);
+            this.btnSubtract.TabIndex = 6;
+            this.btnSubtract.Text = "Subtract";
+            this.btnSubtract.UseVisualStyleBackColor = true;
+            this.btnSubtract.Click += new System.EventHandler(this.btnSubtract_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(501, 24);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(459, 309);
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(983, 24);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(459, 309);
+            this.pictureBox3.TabIndex = 8;
+            this.pictureBox3.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1463, 461);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.btnSubtract);
+            this.Controls.Add(this.btnLoadBackground);
+            this.Controls.Add(this.btnToggleCamera);
+            this.Controls.Add(this.btnUpload);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.Button btnToggleCamera;
+        private System.Windows.Forms.Button btnLoadBackground;
+        private System.Windows.Forms.Button btnSubtract;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ Pagatpatan_DIP2/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+
+namespace Pagatpatan_DIP2
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap greenScreenImage;
+        private Bitmap backgroundImage;
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Act_2 Image Subtraction Pagatpatan";
+        }
+
+
+        private void btnToggleCamera_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ResizeImageToFitPictureBox(Bitmap originalImage, PictureBox pictureBox)
+        {
+          
+            float aspectRatio = (float)originalImage.Width / originalImage.Height;
+
+            // new picturebox width and height
+            int newWidth = pictureBox.Width;
+            int newHeight = (int)(newWidth / aspectRatio);
+
+            
+            if (newHeight > pictureBox.Height)
+            {
+                newHeight = pictureBox.Height;
+                newWidth = (int)(newHeight * aspectRatio);
+            }
+
+            Bitmap resizedImage = new Bitmap(originalImage, new Size(newWidth, newHeight));
+
+            pictureBox.Image = resizedImage;
+        }
+
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files|*.png;*.jpg;*.jpeg;*.bmp";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                greenScreenImage = new Bitmap(openFileDialog.FileName);
+                ResizeImageToFitPictureBox(greenScreenImage, pictureBox1);
+            }
+        }
+
+        private void btnLoadBackground_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files|*.png;*.jpg;*.jpeg;*.bmp";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                backgroundImage = new Bitmap(openFileDialog.FileName);
+                ResizeImageToFitPictureBox(backgroundImage, pictureBox2);
+            }
+        }
+
+        private void btnSubtract_Click(object sender, EventArgs e)
+        {
+            if (greenScreenImage == null || backgroundImage == null)
+            {
+                MessageBox.Show("Please upload both images before subtracting.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            Bitmap resultImage = SubtractImages(greenScreenImage, backgroundImage);
+            ResizeImageToFitPictureBox(resultImage, pictureBox3);
+        }
+
+        private Bitmap SubtractImages(Bitmap greenScreenImage, Bitmap backgroundImage)
+        {
+            Bitmap resultImage = new Bitmap(greenScreenImage.Width, greenScreenImage.Height);
+
+            for (int x = 0; x < greenScreenImage.Width; x++)
+            {
+                for (int y = 0; y < greenScreenImage.Height; y++)
+                {
+                    Color greenScreenPixel = greenScreenImage.GetPixel(x, y);
+                    Color backgroundPixel = backgroundImage.GetPixel(x, y);
+
+                    // 0,0,255
+                    if (greenScreenPixel.R != 0 || greenScreenPixel.G != 255 || greenScreenPixel.B != 0)
+                    {
+                        resultImage.SetPixel(x, y, greenScreenPixel);
+                    }
+                    else
+                    {
+                        resultImage.SetPixel(x, y, backgroundPixel);
+                    }
+                }
+            }
+
+            return resultImage;
+        }
+    }
+}
